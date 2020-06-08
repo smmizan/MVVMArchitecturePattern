9db468a89363f0fb0c7348316c45ed4c3c45fa19
unknown <smmizancse@gmail.com>
2020-06-08 17:19:30 +0600

    add package

--- app/src/main/java/com/smmizan/mvvmarchitectpatern/view/MainActivity.java
+++ app/src/main/java/com/smmizan/mvvmarchitectpatern/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.smmizan.mvvmarchitectpatern;
+package com.smmizan.mvvmarchitectpatern.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.smmizan.mvvmarchitectpatern.R;
 
 public class MainActivity extends AppCompatActivity {
 
